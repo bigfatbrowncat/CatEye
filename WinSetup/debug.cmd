035f79b1e2bb76c5b7afaff3f1d8aa31f65532ad
Igor Lysakov <furyosdragon@gmail.com>
2011-05-29 23:29:37 +0400

    Added get build information and file properties. Code cleaned from comments. Signed-off-by: furyosdragon <furyofdragon@gmail.com>

--- WinSetup/debug.cmd
+++ WinSetup/debug.cmd
@@ -1,1 +1,4 @@
+del ..\bin\Debug\CatEye-*.exe
+del ..\bin\Debug\Version.txt
+"%ProgramFiles%\nsis\makensis" /Dconfig="Debug" GetVersion.nsi
 "%ProgramFiles%\nsis\makensis" /Dconfig="Debug" CatEyeSetup.nsi
