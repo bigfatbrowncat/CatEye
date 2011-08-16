7d33eea7746c054eaae8190f7d084905aec5041b
Ilya Mizus <bigfatbrowncat@gmail.com>
2011-08-16 18:16:38 +0400

    Dividing of the main program into Editor and Queue modes is started.

--- CatEye.UI.Gtk/Main.cs
+++ CatEye.UI.Gtk/Main.cs
@@ -13,11 +13,12 @@ namespace CatEye
 		public static string APP_NAME = "CatEye";
 
 		public static ExtendedStage stage;
-		public static RenderingQueue rq;
 
 		public static MainWindow win;
-		public static RenderingQueueWindow rqwin;
-			
+
+		private static bool mQuitFlag = false;
+		private static int mDelayBeforeUpdate = 100;
+
 		public static readonly Type[] mStageOperationTypes = new Type[]
 		{
 			typeof(CompressionStageOperation),
@@ -104,8 +105,6 @@ namespace CatEye
 			return FloatBitmapGtk.FromPPM(ppl, callback);
 		}
 		
-		private static bool mQuitFlag = false;
-		private static int mDelayBeforeUpdate = 100;
 		public static void Quit()
 		{
 			mQuitFlag = true;
@@ -135,146 +134,212 @@ namespace CatEye
 			return res.ToArray();
 		}
 		
-		public static void AddToQueue(Stage stage, string src, string dest, string dest_type)
-		{
-			Stage stg = new Stage(StageOperationFactory, 
-				StageOperationParametersFactoryFromID,
-				ImageLoader);
-			
-			for (int i = 0; i < stage.StageQueue.Length; i++)
-			{
-				stg.Add((StageOperationParameters)stage.StageQueue[i].Clone());
-			}
-			
-			rq.Add(stg, src, dest, dest_type);
-		}
-		
 		public static void Main (string[] args)
 		{
 			Application.Init ();
 			
-			// Initializing rendering queue and it's window
-			rq = new RenderingQueue();
-			rq.ItemRendering += HandleRqItemRendering;
-			
-			rqwin = new RenderingQueueWindow(rq);
-			rqwin.Visible = false;
-
-			// Initializing stage and main window
-			stage = new ExtendedStage(
-				StageOperationFactory, 
-				StageOperationParametersFactoryFromID,
-				StageOperationParametersEditorFactory, 
-				StageOperationHolderFactory, 
-				ImageLoader);
-			
-		
-			DateTime lastUpdateQueuedTime = DateTime.Now;
-			
-			stage.UpdateQueued += delegate {
-				lastUpdateQueuedTime = DateTime.Now;
-			};
-			
-			win = new MainWindow (stage, mStageOperationTypes);
-			win.Show ();
-			
-			bool just_started = true;
-			
-			while (!mQuitFlag)
+			List<string> argslist = new List<string>(args);
+			if (argslist.Count > 0 && (argslist[0] == "--queue" || argslist[0] == "-q"))
 			{
-				if (just_started)
+				bool queue_server_created = RemotingObject.RunQueueServiceOrConnectToIt();
+				
+				if (argslist.Count > 1)
 				{
-					just_started = false;
-					// Parsing command line arguments
-					if (args.Length > 0)
+					// Trying to load something
+					
+					string cestage_filename = null, raw_filename = null, output_filename = null, type = "jpeg";
+					int inx = argslist.IndexOf("--cestage");
+					if (inx > -1)
 					{
-						if (args.Length == 1)
+						try
 						{
-							bool exists = System.IO.File.Exists(args[0]);
-							bool is_cestage = System.IO.Path.GetExtension(args[0]).ToLower() == ".cestage";
-							bool is_raw = DCRawConnection.IsRaw(args[0]);
-							if (!exists)
-							{
-								Gtk.MessageDialog md = new Gtk.MessageDialog(win, DialogFlags.Modal,
-								                                             MessageType.Error, ButtonsType.Ok, 
-								                                             "Can not find file \"{0}\".", args[0]);
-								md.Title = APP_NAME;
-								md.Run();
-								md.Destroy();
-							}
-							else if (is_cestage)
-							{
-								// Loading cestage
-								stage.LoadStage(args[0]);
-								
-								// Trying to find a proper RAW file
-								string[] raws = FindRawsForCEStage(args[0]);
-								
-								if (raws.Length > 0)
-								{
-									string raw_file = raws[0]; // TODO: support more than one found RAW file
-									
-									Gtk.MessageDialog ask_raw = new Gtk.MessageDialog(win, DialogFlags.Modal,
-									                                             MessageType.Question, ButtonsType.YesNo, 
-									                                             "The raw file \"{0}\" found in the same folder.\nWould you like to open it?", 
-																				 System.IO.Path.GetFileName(raw_file));
-									ask_raw.Title = APP_NAME;
-									bool yes = false;
-									if (ask_raw.Run() == (int)Gtk.ResponseType.Yes) yes = true;
-									ask_raw.Destroy();
-									
-									if (yes) stage.LoadImage(raw_file, Stage.PreScale);
-								}
-							}
-							else if (is_raw)
-							{
-								// TODO: handle raw file in command-line arguments
-							}
-							else // exists, but neither cestage nor raw
-							{
-								Gtk.MessageDialog md = new Gtk.MessageDialog(win, DialogFlags.Modal,
-								                                             MessageType.Error, ButtonsType.Ok, 
-								                                             "Incorrect file \"{0}\".", args[0]);
-								md.Title = APP_NAME;
-								md.Run();
-								md.Destroy();
-							}
+							cestage_filename = argslist[inx + 1];
+						}
+						catch (Exception)
+						{
+							Console.WriteLine("Incorrect cestage argument. It should be \"--cestage <file_name>\"");
+							return;
+						}
+					}
+					inx = argslist.IndexOf("--raw");
+					if (inx > -1)
+					{
+						try
+						{
+							raw_filename = argslist[inx + 1];
+						}
+						catch (Exception)
+						{
+							Console.WriteLine("Incorrect raw argument. It should be \"--raw <file_name>\"");
+							return;
+						}
+					}
+					inx = argslist.IndexOf("--output");
+					if (inx > -1)
+					{
+						try
+						{
+							output_filename = argslist[inx + 1];
+						}
+						catch (Exception)
+						{
+							Console.WriteLine("Incorrect output argument. It should be \"--output <file_name>\"");
+							return;
+						}
+					}
+					inx = argslist.IndexOf("--type");
+					if (inx > -1)
+					{
+						try
+						{
+							type = argslist[inx + 1];
+						}
+						catch (Exception)
+						{
+							Console.WriteLine("Incorrect type argument. It should be \"--type (jpeg, png, bmp)\"");
+							return;
 						}
 					}
 					
+					// Guessing missed arguments
+					if (cestage_filename == null && raw_filename != null)
+					{
+						cestage_filename = System.IO.Path.ChangeExtension(raw_filename, ".cestage");
+					}
+					if (raw_filename == null && cestage_filename != null)
+					{
+						try
+						{
+							raw_filename = FindRawsForCEStage(cestage_filename)[0];
+						}
+						catch (Exception)
+						{
+							Console.WriteLine("No RAW files found for " + cestage_filename);
+							return;
+						}
+					}
+					if (output_filename == null && cestage_filename != null)
+					{
+						output_filename = System.IO.Path.ChangeExtension(cestage_filename, ".jpeg");
+					}
+					
+					if (cestage_filename == null || raw_filename == null || output_filename == null)
+					{
+						Console.WriteLine("Incorrect parameters");
+						return;
+					}
+
+					// Initializing rendering queue and it's window
+					
+					Stage stage = new Stage(StageOperationFactory, StageOperationParametersFactoryFromID, ImageLoader);
+					stage.LoadStage(cestage_filename);
+					RemotingObject.rob.rq.Add(stage, raw_filename, output_filename, type);
 				}
-				
-				Gtk.Application.RunIteration();
-				if ((DateTime.Now - lastUpdateQueuedTime).TotalMilliseconds > mDelayBeforeUpdate)
+				if (queue_server_created)
 				{
-					stage.ProcessQueued();
+					
+					while (!mQuitFlag)
+					{
+						Gtk.Application.RunIteration();
+					}
+				}
+			}
+			else
+			{	
+				// ** Starting main editor window **
+				
+				// Initializing stage and main window
+				stage = new ExtendedStage(
+					StageOperationFactory, 
+					StageOperationParametersFactoryFromID,
+					StageOperationParametersEditorFactory, 
+					StageOperationHolderFactory, 
+					ImageLoader);
+				
+			
+				DateTime lastUpdateQueuedTime = DateTime.Now;
+				
+				stage.UpdateQueued += delegate {
+					lastUpdateQueuedTime = DateTime.Now;
+				};
+				
+				win = new MainWindow (stage, mStageOperationTypes);
+				win.Show ();
+				
+				bool just_started = true;
+				
+				while (!mQuitFlag)
+				{
+					if (just_started)
+					{
+						just_started = false;
+						// Parsing command line arguments
+						if (args.Length > 0)
+						{
+							if (args.Length == 1)
+							{
+								bool exists = System.IO.File.Exists(args[0]);
+								bool is_cestage = System.IO.Path.GetExtension(args[0]).ToLower() == ".cestage";
+								bool is_raw = DCRawConnection.IsRaw(args[0]);
+								if (!exists)
+								{
+									Gtk.MessageDialog md = new Gtk.MessageDialog(win, DialogFlags.Modal,
+									                                             MessageType.Error, ButtonsType.Ok, 
+									                                             "Can not find file \"{0}\".", args[0]);
+									md.Title = APP_NAME;
+									md.Run();
+									md.Destroy();
+								}
+								else if (is_cestage)
+								{
+									// Loading cestage
+									stage.LoadStage(args[0]);
+									
+									// Trying to find a proper RAW file
+									string[] raws = FindRawsForCEStage(args[0]);
+									
+									if (raws.Length > 0)
+									{
+										string raw_file = raws[0]; // TODO: support more than one found RAW file
+										
+										Gtk.MessageDialog ask_raw = new Gtk.MessageDialog(win, DialogFlags.Modal,
+										                                             MessageType.Question, ButtonsType.YesNo, 
+										                                             "The raw file \"{0}\" found in the same folder.\nWould you like to open it?", 
+																					 System.IO.Path.GetFileName(raw_file));
+										ask_raw.Title = APP_NAME;
+										bool yes = false;
+										if (ask_raw.Run() == (int)Gtk.ResponseType.Yes) yes = true;
+										ask_raw.Destroy();
+										
+										if (yes) stage.LoadImage(raw_file, Stage.PreScale);
+									}
+								}
+								else if (is_raw)
+								{
+									// TODO: handle raw file in command-line arguments
+								}
+								else // exists, but neither cestage nor raw
+								{
+									Gtk.MessageDialog md = new Gtk.MessageDialog(win, DialogFlags.Modal,
+									                                             MessageType.Error, ButtonsType.Ok, 
+									                                             "Incorrect file \"{0}\".", args[0]);
+									md.Title = APP_NAME;
+									md.Run();
+									md.Destroy();
+								}
+							}
+						}
+						
+					}
+					
+					Gtk.Application.RunIteration();
+					if ((DateTime.Now - lastUpdateQueuedTime).TotalMilliseconds > mDelayBeforeUpdate)
+					{
+						stage.ProcessQueued();
+					}
 				}
 			}
 		}
 
-		static void HandleRqItemRendering (object sender, RenderingTaskEventArgs e)
-		{
-			FloatBitmapGtk renderDest = (FloatBitmapGtk)e.Target.Stage.CurrentImage;
-			
-			// Drawing to pixbuf and saving to file
-			using (Gdk.Pixbuf rp = new Gdk.Pixbuf(Gdk.Colorspace.Rgb, false, 8, renderDest.Width, renderDest.Height))
-			{
-	
-				renderDest.DrawToPixbuf(rp, 
-					delegate (double progress) {
-					
-						// TODO: report progress via RenderingTask (needed to create an event)
-						//rpw.SetStatusAndProgress(progress, "Saving image...");
-						while (Application.EventsPending()) Application.RunIteration();
-						return true;
-					}
-				);
-			
-				// TODO Can't be used currently cause of buggy Gtk#
-				//rp.Savev(filename, type, new string[] { "quality" }, new string[] { "95" });
-		
-				rp.Save(e.Target.Destination, e.Target.FileType);
-			}
-		}
 	}
 }
